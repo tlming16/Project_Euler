256f0fbf1496dcf527ac32333c0d08ea2f440ca0
mathm <tlming16@fudan.edu.cn>
2020-07-21 12:07:00 +0800

    add cn.cpp -fexec-charset=GBK -finput-charset=UTF-8

---
+++ util/cn.cpp
@@ -0,0 +1,13 @@
+# include <iostream> 
+using namespace std; 
+/*
+-fexec-charset=GBK
+-finput-charset=UTF-8
+g++ -fexec-charset=GBK -finput-charset=UTF-8  cn.cpp 
+*/
+
+int main(){
+
+  cout<<"中文\n";
+  return 0;
+}
